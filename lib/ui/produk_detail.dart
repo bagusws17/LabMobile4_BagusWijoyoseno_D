3cfc81f6e0366b742ab39844539e3087f5e9f057
Bagus <113045284+bagusws17@users.noreply.github.com>
2024-10-09 21:34:25 +0700

    Tugas 5

---
+++ lib/ui/produk_detail.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:tokokita/bloc/produk_bloc.dart';
+import 'package:tokokita/model/produk.dart';
+import 'package:tokokita/ui/produk_form.dart';
+import 'package:tokokita/ui/produk_page.dart';
+import 'package:tokokita/widget/success_dialog.dart';
+import 'package:tokokita/widget/warning_dialog.dart';
+
+// ignore: must_be_immutable
+class ProdukDetail extends StatefulWidget {
+  Produk? produk;
+  ProdukDetail({Key? key, this.produk}) : super(key: key);
+  @override
+  _ProdukDetailState createState() => _ProdukDetailState();
+}
+
+class _ProdukDetailState extends State<ProdukDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail Produk'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Kode : ${widget.produk!.kodeProduk}",
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            Text(
+              "Nama : ${widget.produk!.namaProduk}",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            Text(
+              "Harga : Rp. ${widget.produk!.hargaProduk.toString()}",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            _tombolHapusEdit()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _tombolHapusEdit() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        OutlinedButton(
+          child: const Text("EDIT"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProdukForm(
+                  produk: widget.produk!,
+                ),
+              ),
+            );
+          },
+        ),
+        OutlinedButton(
+          child: const Text("DELETE"),
+          onPressed: () => confirmHapus(),
+        ),
+      ],
+    );
+  }
+
+  void confirmHapus() {
+    if (widget.produk?.id == null) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => const WarningDialog(
+          description: "ID produk tidak ditemukan, tidak bisa menghapus.",
+        ),
+      );
+      return;
+    }
+
+    AlertDialog alertDialog = AlertDialog(
+      content: const Text("Yakin ingin menghapus data ini?"),
+      actions: [
+        OutlinedButton(
+          child: const Text("Ya"),
+          onPressed: () async {
+            bool success = await ProdukBloc.deleteProduk(
+                id: int.parse(widget.produk!.id!));
+            if (success) {
+              showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (BuildContext context) => SuccessDialog(
+                  description: "Produk berhasil dihapus",
+                  okClick: () {
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => const ProdukPage(),
+                      ),
+                    );
+                  },
+                ),
+              );
+            } else {
+              // Jika penghapusan gagal
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => const WarningDialog(
+                  description: "Hapus gagal, silahkan coba lagi",
+                ),
+              );
+            }
+          },
+        ),
+        OutlinedButton(
+          child: const Text("Batal"),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ],
+    );
+    showDialog(builder: (context) => alertDialog, context: context);
+  }
+}
